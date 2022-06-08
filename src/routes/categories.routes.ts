81cffd8e0c4b66176659121c2e46ad7cf7c1a252
Rafael Freitas <vidarafael@hotmail.com>
2022-06-08 15:44:02 -0300

    create useCases

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,15 @@
 import { Router } from 'express'
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository)
-
-  createCategoryService.execute({ name, description })
-
-  res.status(201).send()
+  return createCategoryController.handle(req, res)
 });
 
 categoriesRoutes.get("/", (req, res) => {
-  res.status(200).send(categoriesRepository.list())
+  return listCategoriesController.handle(req, res)
 })
 
 export { categoriesRoutes }
